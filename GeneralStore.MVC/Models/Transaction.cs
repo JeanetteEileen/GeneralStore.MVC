ef54bc14386bf73c6e9e9b3a5bddb2fbf61a17ca
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-03-27 14:31:47 -0400

    Add product view details. Update home index. Add transaction model, transaction index view, transaction controller

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [ForeignKey("Customer")]
+        [Required]
+        public int CustomerId { get; set; }
+        public virtual Customer customer { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        [Required]
+        public int ProductId { get; set; }
+        public virtual Product product { get; set; }
+        [Required]
+        public int ItemCount { get; set; }
+        public DateTimeOffset DateOfTransaction { get; set; }
+    }
+}
